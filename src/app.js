1414a61e2906d1ddc61bfdb57bcc4bbbf9027595
Ashok <ashok@embin.com>
2022-12-29 18:11:05 +0530

    handle bar (hbs) removed from app js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const hbs = require("hbs");
 require("./db/mongoose");
 const cookieParser = require("cookie-parser");
 // load route
