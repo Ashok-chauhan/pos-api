4019034aa8d5f9b0497e8c764a31e035d9cc1d3f
Ashok <ashok@embin.com>
2023-02-04 19:14:03 +0530

    customer check api added

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -13,6 +13,16 @@ const { protect } = require("../middleware/auth");
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
+router.post("/check", async (req, res) => {
+  console.log(req.body.phone + "###");
+  const customer = await Customer.findOne({ phone: req.body.phone });
+  if (!customer) {
+    return res.status(200).json({ success: false });
+  } else {
+    return res.status(200).json({ success: true, customer });
+  }
+});
+
 router.post("/add", protect, async (req, res) => {
   const pos_id = req.user.id;
   req.body.pos_id = pos_id;
